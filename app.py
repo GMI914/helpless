ccdcf541ec83656724ef96b412bf85fc553c4f29
tsotne <tsotnesharvadze@gmail.com>
2019-09-06 20:25:33 +0400

    changed db structure

--- app.py
+++ app.py
@@ -30,25 +30,30 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
-    posts = db.relationship('Posts', backref='post_author', lazy=True)
-    photo = db.relationship('Photos', backref='photo_author', lazy=True)
+    posts = db.relationship('Post', backref='post_author', lazy=True)
+    gallery_photos = db.relationship('GalleryPhoto', backref='photo_author', lazy=True)
+    avatar = db.Column(db.String(255), nullable=False, default='/static/img/default_p.jpeg')
 
 
-class Posts(UserMixin, db.Model):
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     title = db.Column(db.String(50))
     content = db.Column(db.Text(3000))
     date_posted = db.Column(db.String(20), nullable=False, default=datetime.now().strftime("%m/%d/%Y"))
-    photo = db.relationship('Photos', backref='posts_photo', lazy=True)
+    photo = db.relationship('BlogPhoto', backref='posts_photo', lazy=True)
 
 
-class Photos(UserMixin, db.Model):
+class BlogPhoto(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False, default='/static/img/default_p.jpeg')
-    type = db.Column(db.String(20), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=True)
+
+
+class GalleryPhoto(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False, default='/static/img/default_p.jpeg')
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=True)
 
 
 @login_manager.user_loader
@@ -94,7 +99,7 @@ def f_upload():
 @app.route('/')
 @app.route('/index')
 def index():
-    posts = Posts.query.all()
+    posts = Post.query.all()
     return render_template('index.html', posts=posts)
 
 
@@ -137,7 +142,7 @@ def signup():
 @app.route('/my_posts')
 @login_required
 def my_posts():
-    posts = Posts.query.filter_by(user_id=current_user.id)
+    posts = Post.query.filter_by(user_id=current_user.id)
     return render_template('my_posts.html', posts=posts, current_user=current_user,
                            photo=Photos.query.filter_by(user_id=current_user.id, type='profile').first())
 
@@ -148,10 +153,10 @@ def new_post():
     form = NewPostForm()
 
     if form.validate_on_submit():
-        post = Posts(user_id=current_user.id, title=form.title.data, content=form.content.data)
+        post = Post(user_id=current_user.id, title=form.title.data, content=form.content.data)
         db.session.add(post)
         db.session.commit()
-        cur_post = Posts.query.filter_by(user_id=current_user.id, title=form.title.data).order_by('id')[-1]
+        cur_post = Post.query.filter_by(user_id=current_user.id, title=form.title.data).order_by('id')[-1]
         image = Photos(user_id=current_user.id, post_id=cur_post.id, type='post')
         db.session.add(image)
         db.session.commit()
@@ -181,8 +186,8 @@ def logout():
 @app.route('/delete/<id>')
 @login_required
 def delete(id):
-    if Posts.query.filter_by(id=id, user_id=current_user.id).first():
-        Posts.query.filter_by(id=id).delete()
+    if Post.query.filter_by(id=id, user_id=current_user.id).first():
+        Post.query.filter_by(id=id).delete()
         db.session.commit()
     return redirect(url_for('my_posts'))
 
@@ -191,10 +196,10 @@ def delete(id):
 @login_required
 def edit(id):
     form = NewPostForm()
-    if Posts.query.filter_by(id=id, user_id=current_user.id).first():
-        post = Posts.query.filter_by(id=id).first()
+    if Post.query.filter_by(id=id, user_id=current_user.id).first():
+        post = Post.query.filter_by(id=id).first()
         if request.method == 'POST':
-            post = Posts.query.filter_by(id=id).first()
+            post = Post.query.filter_by(id=id).first()
             post.title = form.title.data
             post.content = form.content.data
             db.session.commit()
@@ -224,11 +229,9 @@ def upload():
 @login_required
 def upload_prof():
     if request.method == 'POST' and 'photo' in request.files:
-        old_photo = Photos.query.filter_by(user_id=current_user.id, type='profile').first()
-        if old_photo.name == '/static/img/default_p.jpeg':
-            new_photo = photos.save(request.files['photo'],
-                                    name=str(Photos.query.filter_by(user_id=current_user.id,
-                                                                    type='profile').first().id) + '.jpeg')
+        old_photo = current_user.avatar #@TODO have problems, need fix
+        if old_photo == '/static/img/default_p.jpeg':
+            new_photo = photos.save(request.files['photo'], name=)
             old_photo.name = '/static/img/' + new_photo
             db.session.commit()
         else:
@@ -276,8 +279,7 @@ def del_img(id):
 @app.route('/edit_prof')
 @login_required
 def edit_prof():
-    return render_template('edit_prof.html',
-                           photo=Photos.query.filter_by(user_id=current_user.id, type='profile').first())
+    return render_template('edit_prof.html', photo=current_user.avatar)
 
 
 if __name__ == '__main__':
